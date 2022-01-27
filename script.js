726917cd2c4390c63c32a6f93cb049fd9d9c88ec
Alex <webmaster@papagai.de>
2022-01-27 21:55:14 +0100

    create var, alert, console.log

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Добрый день");
+
+const title = "First";
+const screen = "Blue";
+let screenPrice = 200;
+const rollback = "опасно";
+let fullPrice = 500;
+const adaptive = "адаптивный";
+
+
+console.log("Осторожно",rollback);
