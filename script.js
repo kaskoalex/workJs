719156eb60475572c7a8afca09850cd1ececff67
Alex <webmaster@papagai.de>
2022-01-31 16:04:33 +0100

    create lesson03

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 "use strict";
 
+let rollback = 20;
+let title = prompt("Как называется ваш проект?");
 
-const title = "Lesson JS";
-const screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 200;
-const rollback = 20;
-let fullPrice = 500;
-const adaptive = false;
+let screens = prompt("Какие типы экранов нужно разработать? - Простые, Сложные, Интерактивные");
 
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
 
-console.log('Тип переменной:',typeof title);
-console.log('Тип переменной:',typeof fullPrice);
-console.log('Тип переменной:',typeof adaptive);
+let adaptive = prompt("Нужен ли адаптив на сайте? Да или Нет");
+if (adaptive.toLowerCase() === "да") {
+  adaptive = true;
+} else {
+  adaptive = false;
+}
 
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
 
-console.log('Длинна строки:',screens.length);
-
-
-console.log("Стоимость верстки экранов", screenPrice, "долларов.");
-console.log("Стоимость разработки сайта", fullPrice, "долларов.");
-
-
-console.log(screens.toLowerCase().split(', '));
+let service2 = prompt("Какой ещё дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 
 
-console.log("Процент отката посреднику за работу", fullPrice * rollback / 100);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2
+
+
+let fullPriceRollback = fullPrice*(100-rollback)/100;
+let servicePercentPrice = Math.ceil(fullPriceRollback );
+
+console.log("Итоговая стоимость работы", servicePercentPrice, "рублей.");
+
+
+
+switch (true) {
+  case fullPrice > 30000:
+    console.log("Даем скидку в 10%");
+    break
+
+  case 15000 <= fullPrice && fullPrice<30000:
+    console.log("Даем скидку в 5%");
+    break
+  case fullPrice > 0 && fullPrice <15000:
+    console.log("Скидка не предусмотрена");
+    break  
+  case fullPrice < 0 :
+    console.log("Что то пошло не так");
+    break   
+}  
+
+
