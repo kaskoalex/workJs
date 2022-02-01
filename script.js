889b32afbcb0a3f58d72ab7ff144a5b372bf79ed
Alex <webmaster@papagai.de>
2022-02-01 20:20:52 +0100

    create lesson04

--- script.js
+++ script.js
@@ -2,17 +2,9 @@
 
 let rollback = 20;
 let title = prompt("Как называется ваш проект?");
-
-let screens = prompt("Какие типы экранов нужно разработать? - Простые, Сложные, Интерактивные");
-
+let screens = prompt("Какие типы экранов нужно разработать? - " ,"Простые, Сложные, Интерактивные");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-
 let adaptive = prompt("Нужен ли адаптив на сайте? Да или Нет");
-if (adaptive.toLowerCase() === "да") {
-  adaptive = true;
-} else {
-  adaptive = false;
-}
 
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
@@ -20,31 +12,61 @@ let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой ещё дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-
-
-let fullPriceRollback = fullPrice*(100-rollback)/100;
-let servicePercentPrice = Math.ceil(fullPriceRollback );
-
-console.log("Итоговая стоимость работы", servicePercentPrice, "рублей.");
+let fullPrice
+let allServicePrices
+let servicePercentPrice 
 
 
+const getAllServicePrices = function() {
+  allServicePrices= servicePrice1 + servicePrice2
+  return allServicePrices
+}
 
-switch (true) {
-  case fullPrice => 30000:
-    console.log("Даем скидку в 10%");
-    break
+const getFullPrice= function(){
+  fullPrice = screenPrice + getAllServicePrices()
+  return fullPrice
+}
 
-  case 15000 <= fullPrice && fullPrice<30000:
-    console.log("Даем скидку в 5%");
-    break
-  case fullPrice > 0 && fullPrice <15000:
-    console.log("Скидка не предусмотрена");
-    break  
-  case fullPrice <= 0 :
-    console.log("Что то пошло не так");
-    break   
-}  
+const getTitle = function () {
+  title = title.trim()
+  title = title[0].toUpperCase() + (title.slice(1)).toLowerCase();
+  return title
+}
+
+const getServicePercentPrice = function () {
+  servicePercentPrice = Math.ceil(getFullPrice() * (100 - rollback) / 100)
+  return servicePercentPrice
+
+}
+
+let showTypeOf = function (variable) {
+  return typeof variable;
+} 
 
 
+
+const getRollbackMessage = function (price) {
+  switch (true) {
+    case price >= 30000:
+      return "Даем скидку в 10%"
+    case 15000 <= price && price < 30000:
+      return "Даем скидку в 5%"
+    case price > 0 && price < 15000:
+      return "Скидка не предусмотрена"
+    case price <= 0:
+      return "Что то пошло не так"
+  }
+}
+
+if (adaptive.toLowerCase() === "да") {
+  adaptive = true;
+} else {
+  adaptive = false;
+}
+
+console.log(showTypeOf(getTitle()));
+console.log(showTypeOf(screenPrice));
+console.log(showTypeOf(adaptive));
+console.log(screens.toString());
+console.log("Итоговая стоимость работы", getServicePercentPrice(), "рублей.");
+console.log(getRollbackMessage(getFullPrice()))
